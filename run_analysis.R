4234069446544227b8970ae82bc940ba2e697ee1
mpgreg <milesper@gmail.com>
2015-06-21 23:08:42 +0200

    part5

--- run_analysis.R
+++ run_analysis.R
@@ -10,14 +10,16 @@ library(dplyr)
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 trueFileMD5 <- "d29710c9530a31f303801b6bc34bd895"
 trueFileVersion <- "1.0"
-baseDir <- "~/Documents/School/coursera/data science/getting and cleaning data/project"
+##setwd("~/Documents/School/coursera/data science/getting and cleaning data/project")
+baseDir <- getwd()
 zipFile <- paste(baseDir,"/getdata-projectfiles-UCI HAR Dataset.zip", sep="")
 zipFileList <- list()
 dateDownloaded <- NULL
 
 dataDir <- "UCI HAR Dataset"
-testDir <- paste(dataDir, "/test", sep="")
-trainDir <- paste(dataDir, "/train", sep="")
+##testDir <- paste(dataDir, "/test", sep="")
+##trainDir <- paste(dataDir, "/train", sep="")
+outputFile <- paste(baseDir,"/getdata-project-output.txt", sep="")
 
 ##If the file doesn't already exist in baseDir download it and check md5
 if(!file.exists(zipFile)) {
@@ -29,9 +31,6 @@ if(!file.exists(zipFile)) {
                         This script may not work as originally intended.")
         }
 }
-
-
-
 
 
 ## 1. Merges the training and the test sets to create one data set.  
@@ -63,7 +62,6 @@ for(i in c("test","train")) {
 }
 
 
-
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
 mergeDF <- mergeDF[,c(1,2,grep("std\\(\\)|mean\\(\\)",names(mergeDF)))]
@@ -74,15 +72,28 @@ mergeDF <- mergeDF[,c(1,2,grep("std\\(\\)|mean\\(\\)",names(mergeDF)))]
 activity_labelsDF <- read.table(unz(zipFile, "UCI HAR Dataset/activity_labels.txt"), row.names=NULL)  
 mergeDF$activity <- as.character(factor(mergeDF$activity, labels=activity_labelsDF[,2]))
 
+
 ## 4. Appropriately labels the data set with descriptive variable names. 
 
-##See above (line ~50).  To me it makes more sense to have done this previously.
+##need to remove "-" and "()" from column names
+cNames<-colnames(mergeDF)
+cNames<-gsub("-","\\.",cNames)
+cNames<-gsub("\\(\\)","",cNames)
+colnames(mergeDF) <- cNames
+
 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of 
 ##    each variable for each activity and each subject.
 
-##The "wide" form of the data has 180 rows (and a number of columns equal to the features kept + 2 (one for subject, one for activities)).
+##The "wide" form of the data has 180 rows (and a number of columns equal to the features kept + 2 
+##      (one for subject, one for activities)).
+##The typical long form would be organised with the columns subject, activity, measure, mean but contain 
+##      the same actual numbers for the aggregate steps.
 
-##summaryDF <-group_by(mergeDF,subject)
+meltDF<-melt(mergeDF,id.vars=c("subject","activity"))
+summaryDF<-dcast(meltDF,subject+activity~...,mean)
 
+
+##output data set as a txt file created with write.table() using row.name=FALSE
+write.table(summaryDF, file=outputFile, row.name=FALSE)
 
